70a7b0fe57fc3eb22f0aae23cd9ad7cd010be193
abc <you@example.com>
2019-10-27 23:41:15 +0000

    Updates

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ const startVideo = () => {
     })
 }
 
+
+
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromDisk('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromDisk('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromDisk('./models'),
+  faceapi.nets.faceExpressionNet.loadFromDisk('./models')
 ]).then(startVideo)
 
 
